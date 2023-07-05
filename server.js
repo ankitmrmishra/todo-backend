4d5c92ae64b16bed7d8c4245b1d8b943b69b5de5
ANKIT MISHRA <ankitmrmishra1118@gmail.com>
2023-07-05 15:23:33 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors'); 
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+let todos = [];
+
+app.get('/todos', (req, res) => {
+  res.json(todos);
+});
+
+app.get('/todos/:index', (req, res) => {
+  const todo = todos.find(t => t.index === parseInt(req.params.index));
+  if (!todo) {
+    res.status(404).send();
+  } else {
+    res.json(todo);
+  }
+});
+
+app.post('/todos', (req, res) => {
+  const newTodo = {
+    index: Math.floor(Math.random() * 1000000), // unique random index
+    title: req.body.title,
+    
+  };
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
+
+
+
+app.delete('/todos/:index', (req, res) => {
+  const todoIndex = todos.findIndex(t => t.index === parseInt(req.params.index));
+  if (todoIndex === -1) {
+    res.status(404).send();
+  } else {
+    todos.splice(todoIndex, 1);
+    res.status(200).send();
+  }
+});
+
+// for all other routes, return 404
+app.use((req, res, next) => {
+  res.status(404).send();
+});
+app.listen(3000 , () =>{
+  console.log("listenih");
+})
+module.exports = app;
